c97cf555e762436215dc27bd2f7d6fdeac7b832d
Uli Köhler <ukoehler@techoverflow.net>
2023-03-13 00:59:52 +0100

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+from .bulk_hide_silkscreen_designators_action import BulkHideSilkscreenDesignators
+BulkHideSilkscreenDesignators().register() # Instantiate and register to Pcbnew
---
+++ bulk_hide_silkscreen_designators_action.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import pcbnew
+import os
+import wx
+
+
+class BulkHideSilkscreenDesignators(pcbnew.ActionPlugin):
+    def defaults(self):
+        self.name = "Bulk hide silkscreen designators"
+        self.category = "Silkscreen"
+        self.description = "Hide all silkscreen reference designators for selected footprints"
+        self.show_toolbar_button = True
+        self.icon_file_name = os.path.join(os.path.dirname(__file__), 'icon.png')
+
+    def Run(self):
+        # Filter just selected footprints
+        
+        selected_footprints: list[pcbnew.FOOTPRINT] = [
+            footprint for footprint in pcbnew.GetCurrentSelection()
+            if type(footprint).__name__ == 'FOOTPRINT'
+        ]
+        
+        if len(selected_footprints) == 0:
+            # Show info dialog
+            dlg = wx.MessageDialog(None, "Please select one or multiple footprints!\n...or use Ctrl+A to select everything.", "No footprints selected", wx.OK | wx.ICON_ERROR)
+            dlg.ShowModal()
+            dlg.Destroy()
+
+        for selected_footprint in selected_footprints:
+            # Hide reference
+            reference = selected_footprint.Reference()
+            if reference:
+                reference.SetVisible(False)
+        pcbnew.Refresh()
