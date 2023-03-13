d6a6be577e657cf52e0f04b656b824b4ee290c2f
Uli Köhler <ukoehler@techoverflow.net>
2023-03-13 03:22:17 +0100

    Added 64x64 icon

--- build-package.sh
+++ build-package.sh
@@ -2,6 +2,6 @@
 rm -rf build # remove old build directory
 mkdir -p build/resources build/plugins
 cp metadata.json build
-cp *.py build/plugins
-cp -r icon.png build/resources/
+cp *.py icon.png build/plugins
+cp -r icon-64x64.png build/resources/icon.png
 cd build && zip -r ../kicad-package.zip *
