1806198a928196de3a92bb77b68f19d40e89da1a
shekhuverma <shekharvrm47@gmail.com>
2023-05-13 12:09:37 +0530

    Added support for toggle and replacing reference by values

--- bulk_hide_silkscreen_designators_action.py
+++ bulk_hide_silkscreen_designators_action.py
@@ -2,7 +2,6 @@
 import pcbnew
 import os.path
 import wx
-
 class BulkHideSilkscreenDesignators(pcbnew.ActionPlugin):
     def defaults(self):
         self.name = "Bulk hide silkscreen designators"
@@ -25,9 +24,48 @@ class BulkHideSilkscreenDesignators(pcbnew.ActionPlugin):
             dlg.Destroy()
             return
 
+        dlg = wx.RichMessageDialog(None, "Do you want to Replace Reference by values?","Choose an option",wx.YES | wx.CANCEL_DEFAULT)
+        # dlg.ShowModal() # return value ignored as we have "Ok" only anyhow
+
+        if dlg.ShowModal()==wx.ID_YES:
+            #To move fab layer to silkscreen and sickscreen layer to fab
+            #To show the values instead of component reference
+            for selected_footprint in selected_footprints:
+                value=selected_footprint.Value()
+                reference=selected_footprint.Reference()
+
+                if value.GetLayerName()=="F.Fab":
+                    #Setting the layer to F_Fab
+                    reference.SetLayer(49)
+                    #Setting the layer to F_SilkS
+                    value.SetLayer(37)
+
+                elif value.GetLayerName()=="B.Fab":
+                    #Setting the layer to B_Fab
+                    reference.SetLayer(48)
+                    #Setting the layer to B_SilkS
+                    value.SetLayer(36)
+
+                elif value.GetLayerName()=="F.Silkscreen":
+                    #Setting the layer to F_SilkS
+                    reference.SetLayer(37)
+                    #Setting the layer to F_Fab
+                    value.SetLayer(49)
+
+                elif value.GetLayerName()=="B.Silkscreen":
+                    #Setting the layer to B_SilkS
+                    value.SetLayer(36)
+                    #Setting the layer to B_Fab
+                    reference.SetLayer(48)
+
         for selected_footprint in selected_footprints:
             # Hide reference
             reference = selected_footprint.Reference()
             if reference:
-                reference.SetVisible(False)
+                if reference.IsVisible():
+                    # Hide reference if visible earlier
+                    reference.SetVisible(False)
+                else:
+                    # Show reference if not visible earlier
+                    reference.SetVisible(True)
         pcbnew.Refresh()
